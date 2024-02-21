fa70e66b8feb03cab20058d38ea276b78554336e
VShah2312 <shahvrunda2312@gmail.com>
2024-02-20 22:03:38 -0500

    while_2

--- Python + DSA Course/5. While/While.py
+++ Python + DSA Course/5. While/While.py
@@ -121,12 +121,12 @@ i: int = num * -1
 j: int = num
 
 if i < 0:
-    while i <= num:
+    while i <= j:
         print(i, end=" ")
         i += 1
     print()
 else:
     while j <= i:
-        print(j, end=" ")
-        j += 1
+        print(i, end=" ")
+        i -= 1
     print()
